64a8d67bdf18945a9ed39ecc5d695338a219c488
li0t <dev@liot.studio>
2018-05-25 22:00:44 -0400

    Custom color

--- assets/js/tracker.js
+++ assets/js/tracker.js
@@ -1,6 +1,8 @@
+createCustomColor('redish', '#b37b7b')
+
 function Tracker(videoId) {
   this.videoId = videoId;
-  this._tracker =  new tracking.ColorTracker(['yellow']);
+  this._tracker = new tracking.ColorTracker(['yellow', 'redish']);
 }
 
 Tracker.prototype.start = function start() {
@@ -16,4 +18,43 @@ Tracker.prototype.track = function track(cb) {
       cb(event.data);
     }
   });
+}
+
+function createCustomColor(name, value) {
+  var components = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+  var customColorR = parseInt(components[1], 16);
+  var customColorG = parseInt(components[2], 16);
+  var customColorB = parseInt(components[3], 16);
+
+  var colorTotal = customColorR + customColorG + customColorB;
+
+  if (colorTotal === 0) {
+    tracking.ColorTracker.registerColor(name, function (r, g, b) {
+      return r + g + b < 10;
+    });
+  } else {
+    var rRatio = customColorR / colorTotal;
+    var gRatio = customColorG / colorTotal;
+
+    tracking.ColorTracker.registerColor(name, function (r, g, b) {
+      var colorTotal2 = r + g + b;
+
+      if (colorTotal2 === 0) {
+        if (colorTotal < 10) {
+          return true;
+        }
+        return false;
+      }
+
+      var rRatio2 = r / colorTotal2,
+        gRatio2 = g / colorTotal2,
+        deltaColorTotal = colorTotal / colorTotal2,
+        deltaR = rRatio / rRatio2,
+        deltaG = gRatio / gRatio2;
+
+      return deltaColorTotal > 0.9 && deltaColorTotal < 1.1 &&
+        deltaR > 0.9 && deltaR < 1.1 &&
+        deltaG > 0.9 && deltaG < 1.1;
+    });
+  }
 }
